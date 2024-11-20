3b225d6076239886f1568a3a9465deccb6d0ef01
extaleus <streetworkoutinlenino@gmail.com>
2024-11-20 18:03:42 +0300

    prepare for db

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,29 @@ import (
 	"log"
 
 	"github.com/extaleus/pmanager/pkg/handler"
+	"github.com/extaleus/pmanager/pkg/repository"
+	"github.com/extaleus/pmanager/pkg/service"
 	server "github.com/extaleus/pmanager/server"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init config: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(server.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,11 +1,15 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
-
+	fmt.Println("signUp")
 }
 
 func (h *Handler) signIn(c *gin.Context) {
-
+	fmt.Println("signIn")
 }
